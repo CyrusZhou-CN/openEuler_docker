8eb70d5e363324c21b995481520900f726d20aad
CyrusZhou <lsqtzj@msn.com>
2023-04-05 11:14:48 +0200

    # 更新到 httpd-2.4.56

--- openeuler_httpd/run.bat
+++ openeuler_httpd/run.bat
@@ -1,2 +1,2 @@
-docker run --name openeuler_httpd -p 9980:80 -tid lsqtzj/openeuler_httpd
+docker run --name openeuler_httpd -p 9880:80 -tid lsqtzj/openeuler_httpd
 docker exec -it openeuler_httpd /bin/bash
