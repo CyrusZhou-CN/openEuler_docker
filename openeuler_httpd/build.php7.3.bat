0916d0c4f406a4523e9e865497957c58311ce014
Cyrus Zhou <lsqtzj@msn.com>
2023-11-11 15:42:57 +0100

    添加PHP支持

---
+++ openeuler_httpd/build.php7.3.bat
@@ -0,0 +1,1 @@
+docker build -t lsqtzj/openeuler_httpd:php7.3 -f Dockerfile.php7.3 .
---
+++ openeuler_httpd/run.php7.3.bat
@@ -0,0 +1,2 @@
+docker run --name openeuler_httpd_php -p 9880:80 -tid lsqtzj/openeuler_httpd:php7.3
+docker exec -it openeuler_httpd_php /bin/bash
